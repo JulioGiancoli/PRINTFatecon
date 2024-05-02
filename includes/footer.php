1f2ab12e266e846bf65c2e5279f2f2f897c05bcd
julio g <jgrramalho97@gmail.com>
2024-05-02 16:51:56 -0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="row">
+        <div class="col logofooter">
+            <img  src="images/logoFatecon.png" alt="">
+        </div>
+        <div class="col-5"><ul>
+                    <li><a href="">Início</a></li>
+                    <li><a href="">Em destaque</a></li>
+                    <li><a href="">Artistas</a></li>
+                    <li><a href="">Galeria</a></li>
+                    <li><a href="">História</a></li>
+                    <li><a href="">Eventos</a></li>
+                    <li><a href="">Sobre</a></li>
+                </ul>
+    </div>
+    <div class="col social">
+        <h3 style="color:white;">Nossas Redes</h3>
+        <ul>
+            <li><a href=""><img src="images/face.png" alt=""></a></li>
+            <li><a href=""><img src="images/insta.png" alt=""></a></li>
+            <li><a href=""><img src="images/twitter.png" alt=""></a></li>
+        </ul>
+    </div>
+    </div>
+
+</footer>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jomhuria&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <header>
+    <div class="row fatecon">
+        <div class="col logo">
+            <img  src="images/logoFatecon.png" alt="">
+        </div>
+        <div class="col"></div>
+        <div class="col menu">
+            <div class="dropdown">
+                <button class="btn btn-secondary" id="botao-header" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="icons8-cardápio-50.png" alt="">
+                </button>
+                <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Quadrinhos em destaque</a></li>
+                <li><a class="dropdown-item" href="#">Artistas</a></li>
+                <li><a class="dropdown-item" href="#">Galeria</a></li>
+                <li><a class="dropdown-item" href="#">História</a></li>
+                <li><a class="dropdown-item" href="#">Eventos</a></li>
+                <li><a class="dropdown-item" href="#">Contato</a></li>
+                <li><a class="dropdown-item" href="#">Quem somos</a></li>
+                </ul>
+            </div>
+        </div> 
+    </div>
+        <nav>
+            <a href="" style="border-left:none;">Inicio</a>
+            <a href="">Em destaque</a>
+            <a href="">Artistas</a>
+            <a href="">Galerias</a>
+            <a href="">História</a>
+            <a href="">Eventos</a>
+            <a href="">Contato</a>
+            <a href="">Sobre nós</a>
+        </nav>
+    </header>
