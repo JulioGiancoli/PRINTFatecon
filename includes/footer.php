c3067993428617b737092d4b9434b7c5a39c3090
Amauri Carlos <amauricarlosdecarvalho@gmail.com>
2024-05-04 17:35:37 -0300

    Adicionado o carrousel com imgs e re-organizado a estrutura das paginas

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,4 @@
-<footer>
-    <div class="row">
+<div class="row">
         <div class="col logofooter">
             <img  src="images/logoFatecon.png" alt="">
         </div>
@@ -22,9 +21,3 @@
         </ul>
     </div>
     </div>
-
-</footer>
-</div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</html>
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php include 'includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include 'includes/head.php'; ?>
+    <title>Home</title>
+</head>
+<body>
+    <header>
+    <?php include 'includes/header.php'; ?>
+    </header>
+    <main>
+        <?php include 'includes/carrousel.php'; ?>
+    </main>
 
-<?php include 'includes/footer.php';?>
+
+
+
+    <footer>
+    <?php include 'includes/footer.php';?>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
