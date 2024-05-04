52da1c6650ebcc0bdac3131b9ddec47aa7b557fc
Amauri Carlos <amauricarlosdecarvalho@gmail.com>
2024-05-04 19:27:17 -0300

    Arrumando arquivos

--- index.php
+++ index.php
@@ -5,12 +5,18 @@
     <title>Home</title>
 </head>
 <body>
+    <div class="container text-center">
     <header>
     <?php include 'includes/header.php'; ?>
     </header>
     <main>
         <?php include 'includes/carrousel.php'; ?>
+        <div class="row">
+        <h1 class="container  welc">Bem vindo a Fatecon!</h1>
+        </div>
     </main>
+
+  
 
 
 
@@ -18,6 +24,7 @@
     <footer>
     <?php include 'includes/footer.php';?>
     </footer>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
